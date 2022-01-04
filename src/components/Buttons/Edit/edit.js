416d6f7ee4c4e71b458e70a32a7d0a225d23e48b
BenVonAchen <bennyvonachen@gmail.com>
2022-01-03 18:01:52 -0700

    Added Card and Button components

---
+++ src/components/Buttons/Edit/edit.js
@@ -0,0 +1,12 @@
+import "../../../assets/fonts/fonts.css";
+import "./edit.css";
+
+function EditButton() {
+  return (
+    <div id="edit">
+      <button>Edit Posts</button>
+    </div>
+  );
+}
+
+export default EditButton;
