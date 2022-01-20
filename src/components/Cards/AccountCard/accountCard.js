872009add800373e8f80950156e493ee2500dac6
BenVonAchen <bennyvonachen@gmail.com>
2022-01-20 16:22:06 -0700

    Working on Account Page

--- src/components/Cards/AccountCard/accountCard.js
+++ src/components/Cards/AccountCard/accountCard.js
@@ -1,4 +1,95 @@
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Container from "@mui/material/Container";
+import Grid from "@mui/material/Grid";
+import Typography from "@mui/material/Typography";
+//import TextField from "@mui/material/TextField";
+//import TextareaAutosize from "@mui/material/TextareaAutosize";
+import "../../../assets/fonts/fonts.css";
+import profilepic from "../../../assets/images/bva.jpg";
+//import "./postCard.css";
 import Update from "../../Buttons/Update/update";
+import Create from "../../Buttons/Create/create";
+import Edit from "../../Buttons/Edit/edit";
+import "./accountCard.css";
+import "../../../assets/fonts/fonts.css";
+
+function AccountCard() {
+  return (
+    <div id="accountCard">
+      <Container
+        maxWidth="lg"
+        sx={{
+          borderRadius: 10,
+          bgcolor: "#f7f0a3",
+          padding: 5,
+        }}
+      >
+        <Grid container spacing={3}>
+          <Grid item xs={0} sm={1} />
+          <Grid item xs={12} md={10}>
+            <Typography variant="h2">Account Info</Typography>
+          </Grid>
+          <Grid item xs={0} sm={1} />
+
+          <Grid item xs={0} sm={0.5} />
+          <Grid item xs={12} md={7.5}>
+            <Typography variant="h4">Full Name: Ben Von Achen</Typography>
+            <Typography variant="h4">User Name: BVA1000</Typography>
+            <Typography variant="h4">Email: benvonachen@bva.com</Typography>
+          </Grid>
+          <Grid item xs={12} md={3.5}>
+            <Box sx={{ justifyContent: "center" }}>
+              <img
+                src={profilepic}
+                className="profilePic"
+                alt="Profile Picture"
+                style={{ width: 250, height: 250 }}
+              />
+            </Box>
+          </Grid>
+          <Grid item xs={0} sm={0.5} />
+
+          <Grid item xs={0} sm={1.005} />
+          <Grid
+            item
+            xs={12}
+            md={3.33}
+            spacing={2}
+            style={{ justifyContent: "center" }}
+          >
+            <Update />
+          </Grid>
+
+          <Grid
+            item
+            xs={12}
+            md={3.33}
+            spacing={2}
+            style={{ justifyContent: "center" }}
+          >
+            <Create />
+          </Grid>
+
+          <Grid
+            item
+            xs={12}
+            md={3.33}
+            spacing={2}
+            style={{ justifyContent: "center" }}
+          >
+            <Edit />
+          </Grid>
+          <Grid item xs={0} sm={1.005} />
+        </Grid>
+      </Container>
+    </div>
+  );
+}
+
+export default AccountCard;
+
+/*import Update from "../../Buttons/Update/update";
 import Create from "../../Buttons/Create/create";
 import Edit from "../../Buttons/Edit/edit";
 import "./accountCard.css";
@@ -22,4 +113,4 @@ function AccountCard() {
   );
 }
 
-export default AccountCard;
+export default AccountCard;*/
--- src/pages/Account/accountInfo.js
+++ src/pages/Account/accountInfo.js
@@ -1,12 +1,19 @@
 import AccountCard from "../../components/Cards/AccountCard/accountCard";
+import Grid from "@mui/material/Grid";
 import "../../assets/fonts/fonts.css";
 import "./accountInfo.css";
 
 function AccountInfo() {
   return (
     <div id="accountInfo">
-      <h1>Hello!</h1>
-      <AccountCard />
+      <Grid container spacing={4}>
+        <Grid item xs={12} md={12}>
+          <h1>Hello!</h1>
+        </Grid>
+        <Grid item xs={12} md={12}>
+          <AccountCard />
+        </Grid>
+      </Grid>
     </div>
   );
 }
