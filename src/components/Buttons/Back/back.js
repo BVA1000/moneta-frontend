ee62beddba9897018bc48df7a8298f1362e2f527
BenVonAchen <bennyvonachen@gmail.com>
2022-01-21 16:45:01 -0700

    Cleaned up some unneeded commented code

--- src/components/Buttons/Back/back.js
+++ src/components/Buttons/Back/back.js
@@ -1,7 +1,5 @@
 import * as React from "react";
-//import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
-//import Grid from "@mui/material/Grid";
 import "../../../assets/fonts/fonts.css";
 import "./back.css";
 
--- src/layout/Layout/layout.js
+++ src/layout/Layout/layout.js
@@ -1,7 +1,6 @@
 import "./layout.css";
 import Navigation from "../Navigation/navigation";
 import Footer from "../Footer/footer";
-//import { Grid } from "@mui/material";
 
 function Layout(props) {
   return (
