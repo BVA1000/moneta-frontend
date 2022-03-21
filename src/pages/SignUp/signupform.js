e35599fb82fdb159e798152def5ab2d866f38898
BenVonAchen <bennyvonachen@gmail.com>
2022-03-21 17:25:34 -0600

    Added comments to Sign Up Form

--- src/pages/SignUp/signupform.js
+++ src/pages/SignUp/signupform.js
@@ -1,27 +1,39 @@
-import Submit from "../../components/Buttons/Submit/submit";
-import { Link } from "react-router-dom";
-import "../../assets/fonts/fonts.css";
-import "./signup.css";
+import Submit from '../../components/Buttons/Submit/submit';
+import { Link } from 'react-router-dom';
+import '../../assets/fonts/fonts.css';
+import './signup.css';
 
 //import { useDispatch } from "react-redux";
 //import { createAccount } from "../../../store/accountSlice";
 
-function SignUpForm() {
+function SignUpForm(fullname, email, username, password) {
   //const dispatch = useDispatch()
+  //this.fullname = fullname;
+  //this.email = email;
+  //this.username = username;
+  //this.password = password;
 
   return (
     <div id="signupform">
       <div>
-        <input id="fullname" placeholder="Full Name" type="text" required />
+        <input id="fullname" placeholder="Full Name" type="text" required>
+          {/* fullname */}
+        </input>
       </div>
       <div>
-        <input id="email" placeholder="Email" type="text" required />
+        <input id="email" placeholder="Email" type="text" required>
+          {/* email */}
+        </input>
       </div>
       <div>
-        <input id="username" placeholder="Username" type="text" required />
+        <input id="username" placeholder="Username" type="text" required>
+          {/* username */}
+        </input>
       </div>
       <div>
-        <input id="password" placeholder="Password" type="password" required />
+        <input id="password" placeholder="Password" type="password" required>
+          {/* password */}
+        </input>
       </div>
       <div>
         <input placeholder="Account Type" type="text" required />
